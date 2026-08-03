c099303efa31edd5494a5cf19a0e47027cfc6a5e
Drug <cropflre2333@gmail.com>
2026-08-03 11:08:06 +0800

    chore(ui): remove unused hero import

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 import { Typewriter } from '../ui/typewriter';
 import { getRandomWisdom, isQuotesVisible } from '../../data/quotes';
